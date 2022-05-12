209cb71c7378415d878d8c7b5e2ff14e01faf7f2
igordiasdeandrade <igor.d.andrade@live.com>
2022-05-12 00:48:20 +0000

    definido welcome/index como root do projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :coins
+  resources :coins #define os recursos de get, post, put, delete, todos de uma vez
 
   root to:"welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
